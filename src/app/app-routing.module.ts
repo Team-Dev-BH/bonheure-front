57e3f7de1c4015f1489c1cf7f060679447785f4e
chedli <chedlicris66@yahoo.fr>
2019-09-30 19:56:40 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ServicesComponent } from './components/services/services.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { LoginComponent } from './components/auth/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'register', component : RegisterComponent},
+  {path:'login', component : LoginComponent},
+  {path:'services', component : ServicesComponent}
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { ContentComponent } from './components/layout/content/content.component';
+import { ServicesComponent } from './components/services/services.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { CommandesComponent } from './components/commandes/commandes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ContentComponent
+    ContentComponent,
+    ServicesComponent,
+    RegisterComponent,
+    LoginComponent,
+    CommandesComponent
   ],
   imports: [
     BrowserModule,
